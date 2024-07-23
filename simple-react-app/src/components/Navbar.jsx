a53d1962431c19e7105627ba1050d982754e76ac
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-23 14:14:58 -0400

    enhance Navbar component
    Updated Navbar component with improved structure and styling. Added React logo, adjusted text elements, and ensured proper CSS integration for a polished look.

--- simple-react-app/src/components/Navbar.jsx
+++ simple-react-app/src/components/Navbar.jsx
@@ -1,3 +1,18 @@
+import reactLogo from "../images/logo512.png";
+
+import "../styles.css";
+
 export default function Navbar() {
-  return <h1>Navbar Component</h1>;
+  return (
+    <nav>
+      <img
+        id="react-logo"
+        src={reactLogo}
+        alt="React.js Logo"
+        className="nav--icon"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
